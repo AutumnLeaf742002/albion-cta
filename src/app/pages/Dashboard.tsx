69c4c7214b9cf1cbcf4cbd5222926ac955311e55
Alexis <alexismanuelperezperez742002@gmail.com>
2025-11-18 20:28:23 -0400

    add hr again

--- src/app/pages/Dashboard.tsx
+++ src/app/pages/Dashboard.tsx
@@ -1,11 +1,13 @@
 import { Text } from "../../ui/components/Text"
 import { AppLayout } from "../layout/AppLayout"
+import { Hr } from '../../ui/components/Hr';
 
 export const Dashboard = () => {
     return (
         <AppLayout>
             <Text>
                 Dashboard
+                <Hr />
             </Text>
         </AppLayout>
     )
